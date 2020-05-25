dfc84a78cabdb90307e88878413ba8ec7326ea67
BPabey <buddimalp@gmail.com>
2020-05-25 08:59:25 +0530

    Initial commit

---
+++ app/src/main/java/com/osaka/kotlinlabsheet/Home.kt
@@ -0,0 +1,21 @@
+package com.osaka.kotlinlabsheet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val click = findViewById<Button>(R.id.welcomeBtn)
+        click.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
